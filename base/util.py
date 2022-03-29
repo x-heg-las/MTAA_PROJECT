adb5e0d7166abd572a18b942df32085de9ee3e34
Radovan Zuzula <radovan.zuzula@gmail.com>
2022-03-29 12:52:39 +0200

    uprava util

--- base/util.py
+++ base/util.py
@@ -1,13 +1,13 @@
 def requestToDictionary(request) ->dict:
     output = {"id": request.id, "title": request.title, "user": request.user.id, "answered_by_user": request.answered_by_user,
-              "request_type__name": request.request_type.id, "description": request.description,
+              "request_type__name": request.request_type.name, "description": request.description,
               "call_reqested": request.call_requested, "file": request.file, "created_at": request.created_at,
               "updated_at": request.updated_at}
     return output
 
 
 def userToDictionary(user) ->dict:
-    output = {"id": user.id, "username": user.username, "user_type__name": user.user_type.id,
+    output = {"id": user.id, "username": user.username, "user_type__name": user.user_type.name,
               "full_name": user.full_name, "phone_number": user.phone_number, "created_at": user.created_at,
               "updated_at": user.updated_at}
     if user.profile_img_file:
